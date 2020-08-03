55f963ae408b3ecb2da9244e4fc449d9b84e2515
Viktor Ilievski <viktor.ilievski@students.finki.ukim.mk>
2020-08-03 19:56:10 +0200

    Connected with Firebase Database and getting info from it.

--- app/src/main/java/com/individual/vozenredmk/MenuActivity.java
+++ app/src/main/java/com/individual/vozenredmk/MenuActivity.java
@@ -1,27 +1,45 @@
 package com.individual.vozenredmk;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.renderscript.Sampler;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 
 public class MenuActivity extends AppCompatActivity {
 
+    DatabaseReference mReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("relacii");
-
-        myRef.setValue("Hello, World!");
-
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mReference = FirebaseDatabase.getInstance().getReference().child("relacii");
+        mReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                TextView textView = (TextView)findViewById(R.id.testText);
+                String value = snapshot.child("relacija2").getValue(String.class);
+                textView.setText(value);
 
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 }
